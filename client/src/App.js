3a8442c2538520df89315382c5badfbe25aa486b
mmasima <morapedimasima@gmail.com>
2020-10-06 18:29:23 +0200

    login is grand, working, working on forgetpass

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Registration from './components/registration/registration';
 import VideoPlayer from './components/videoPlayer/videoPlayer';
-import Main from './components/main-page';
+// import Main from './components/mainPage/main-page';
 import Login from './components/login.js/login';
 import Navbar from './components/navbar';
+import ForgotPass from './components/forgotPass/forgotPass';
+import ResetPass from './components/forgotPass/resetPass';
 
 
 class App extends Component {
@@ -18,6 +20,8 @@ class App extends Component {
           <Route component={Login} exact path="/" />
           <Route component={Registration} exact path="/Register" />
           <Route component={VideoPlayer} exact path="/Video" />
+          <Route component={ForgotPass} exact path="/forgotPass" />
+          <Route component={ResetPass} exact path="/resetPass" />
         </Router>
       </div>
     );
