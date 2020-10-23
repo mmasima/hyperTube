9bc3bdaf1ea1e68eb99b0c4a8b9ea40ee2be71f7
mmasima <morapedimasima@gmail.com>
2020-10-23 14:02:28 +0200

    I left commnets in the mainPage

--- client/src/components/mainPage/mainPage.js
+++ client/src/components/mainPage/mainPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { useHistory, withRouter } from 'react-router-dom';
 import auth from '../../config/auth';
 import SearchArea from './SearchArea';
 import MovieList from './MovieList';
@@ -17,16 +18,25 @@ class Main extends Component {
       currentPage: 1,
       currentMovie: null
     }
-    this.apiKey = "0b590acb8e9de1d3c68c2edaf553692c"
+  }
+  // here is the data you need to get the movie torrent
+  download =(props) => {
+    console.log("1 : this is all the movie data you get");
+    console.log(this.state.currentMovie);
+    console.log("2: this is what you were looking for yesterday")
+    //torrent to download
+    console.log(this.state.currentMovie.torrents);
+    //torrent ID
+    console.log(this.state.currentMovie.id)
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
+    fetch(`https://yts.mx/api/v2/list_movies.json=?query_term=${this.state.searchTerm}`)
       .then(data => data.json())
       .then(data => {
-        console.log(data);
-        this.setState({ movies: [...data.results], totalResults: data.total_results })
+        
+        this.setState({ movies: [...data.data.movies], totalResults: data.data.movie_count })
       })
   }
 
@@ -35,17 +45,17 @@ class Main extends Component {
   }
 
   nextPage = (pageNumber) => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}&page=${pageNumber}`)
+    fetch(`https://yts.mx/api/v2/list_movies.json=?query_term=${this.state.searchTerm}&page=${pageNumber}`)
       .then(data => data.json())
       .then(data => {
-        console.log(data);
-        this.setState({ movies: [...data.results], currentPage: pageNumber })
+        
+        this.setState({ movies: [...data.data.movies], currentPage: pageNumber })
       })
   }
    viewMovieInfo = (id) => {
     let filteredMovie;
     this.state.movies.forEach((movie, i) => {
-      if (movie.id === id) {
+      if (movie.id === id.movieId) {
         filteredMovie = movie
       }
     })
@@ -94,7 +104,46 @@ loggedIn = (props) => {
             <MovieList viewMovieInfo={this.viewMovieInfo} movies={this.state.movies} />
           </div>
           :
-          <MovieInfo currentMovie={this.state.currentMovie} closeMovieInfo={this.closeMovieInfo} />
+          <div>
+          <div className="container">
+              <div className="row" onClick={this.closeMovieInfo} style={{ cursor: "pointer", paddingTop: 50 }}>
+                  <i class="fas fa-arrow-left"></i>
+                  <span style={{ marginLeft: 10 }}>Go back</span>
+              </div>
+              <div className="card  text-white bg-secondary mb-3">
+                  <div className="row no-gutters">
+                      <div className="col-md-4">
+                          {this.state.currentMovie.background_image_original == null ? <img className="card-img" src={`https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg`} alt="Card image cap" style={{
+                              width: "100%",
+                              height: 360
+                          }} /> : <img className="" src={this.state.currentMovie.background_image_original} alt="Card image" style={{
+                              width: "100%",
+                              height: 360
+                          }} />}
+                      </div>
+                      <div className="col-md-8">
+                          <div className="card-body">
+                              <h5 className="card-title">{this.state.currentMovie.title}</h5>
+                              <label >year released</label>
+                              <p className="card-text">{this.state.currentMovie.year}</p>
+                              <label htmlFor="">rating</label>
+                              <p className="card-text">{this.state.currentMovie.rating}</p>
+                              <p className="card-text">{this.state.currentMovie.description_full}</p>
+                              <div className="row">
+                                  <div className="col">
+                                      <button className="btn btn-primary" onClick={this.download}>download Movie</button>
+                                  </div>
+                                  <div className="col">
+                                      <button className="btn btn-primary">comment</button>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+      </div>
         }
         {this.state.totalResults > 20 && this.state.currentMovie == null ? <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage} /> : ''}
       </div>
